09c872c9cbb900d890d72e00319d6cd5212ba7c4
vitorborges132007-cyber <vitorborges132007@gmail.com>
2026-05-19 20:54:08 -0300

    Create header file for ArraySequence class
    Define ArraySequence class and Node structure.

---
+++ .h
@@ -0,0 +1,32 @@
+// arquivo.h
+#ifndef ARRAYSEQUENCE_H
+#define ARRAYSEQUENCE_H
+
+#include <string>
+using namespace std;
+
+struct Node {
+    string value;
+    Node* next;
+};
+
+class ArraySequence{
+private:
+    Node* head;
+    int quantity;
+
+public:
+
+    ArraySequence();
+    ~ArraySequence();
+    int size();
+    bool isEmpty();
+    bool contains(string s);
+    bool add(string s, int position);
+    string get(int position);
+    bool remove(int position);
+    void print();
+};
+
+#endif
+
